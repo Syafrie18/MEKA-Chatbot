f4d776a905341acdf5496ad72771238d4094afae
Syafrie18 <139037894+Syafrie18@users.noreply.github.com>
2023-07-10 00:43:29 +0700

    Update TugasAI.py

--- TugasAI.py
+++ TugasAI.py
@@ -32,12 +32,9 @@ def ngomong(text):
     namafile = 'Ngomong.mp3'
     def reading():
         suara = gTTS(text=teks, lang=bahasa, slow=False)
-        suara.save("output.mp3")
-        audio_file_read = open('output.mp3', 'rb')
-        audio_bytes = audio_file_read.read()
-        bin_str = base64.b64encode(audio_bytes).decode()
-        st.audio(audio_bytes, format='audio/mp3')
-        st.markdown(get_binary_file_downloader_html("output.mp3",'Audio File'), unsafe_allow_html=True)
+        suara.save(namafile)
+        os.system(f'start {namafile}')
+        
     reading()
 
 def main():
